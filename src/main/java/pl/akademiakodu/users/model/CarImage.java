c82d2dc16cfff2d54dee7b65b6924213ff63945e
wpij <wpij@wp.pl>
2018-08-18 21:23:02 +0200

    second

---
+++ src/main/java/pl/akademiakodu/users/model/CarImage.java
@@ -0,0 +1,32 @@
+package pl.akademiakodu.users.model;
+
+public class CarImage {
+
+    private String imageurl;
+    private String title;
+    private String description;
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setImageurl(String imageurl) {
+        this.imageurl = imageurl;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
